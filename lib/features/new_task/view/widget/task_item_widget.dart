c3d0948d2c7d745d807c6afadd2a10d170bec11f
Montakim <montakimshuvo@gmail.com>
2026-04-15 11:56:19 +0600

    refactor task_item_widget.dart ui code

--- lib/features/new_task/view/widget/task_item_widget.dart
+++ lib/features/new_task/view/widget/task_item_widget.dart
@@ -58,82 +58,75 @@ class TaskItemWidget extends StatelessWidget {
                     crossAxisAlignment: .start,
                     mainAxisAlignment: .spaceBetween,
                     children: [
+                      GestureDetector(
+                        onTap: () {
+                          controller.toggleTask(
+                            task['id'],
+                            task['is_completed'],
+                          );
+                        },
+                        child: Icon(
+                          task['is_completed'] == 1
+                              ? Icons.check_circle
+                              : Icons.radio_button_unchecked,
+                          color: task['is_completed'] == 1
+                              ? Colors.green
+                              : Colors.grey,
+                        ),
+                      ),
                       // wrap this widget expanded widget
+                      const SizedBox(width: AppConstants.valueDouble12),
+                      // wrap ``GestureDetector`` widget expanded widget
                       Expanded(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                controller.toggleTask(
-                                  task['id'],
-                                  task['is_completed'],
-                                );
-                              },
-                              child: Icon(
-                                task['is_completed'] == 1
-                                    ? Icons.check_circle
-                                    : Icons.radio_button_unchecked,
-                                color: task['is_completed'] == 1
-                                    ? Colors.green
-                                    : Colors.grey,
+                        child: GestureDetector(
+                          onTap: () async {
+                            await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    TaskDetailsScreen(task: task),
                               ),
-                            ),
-                            const SizedBox(width: AppConstants.valueDouble12),
-                            // wrap ``GestureDetector`` widget expanded widget
-                            Expanded(
-                              child: GestureDetector(
-                                onTap: () async {
-                                  await Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          TaskDetailsScreen(task: task),
-                                    ),
-                                  );
-                                  controller.loadTasks();
-                                },
-                                child: Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        task['title'] ?? '',
-                                        style: TextStyle(
-                                          fontSize: AppConstants.valueDouble16,
-                                          decoration: task['is_completed'] == 1
-                                              ? TextDecoration.lineThrough
-                                              : null,
-                                          color: Colors.black,
-                                        ),
-                                      ),
-                                      Text(
-                                        task['notes'] ?? '',
-                                        style: const TextStyle(
-                                          fontSize: AppConstants.valueDouble12,
-                                        ),
-                                      ),
-                                      if (task['due_date'] != null)
-                                        Text(
-                                          DateFormat('EEEE, MMM d, h:mm a').format(
-                                            DateTime.fromMillisecondsSinceEpoch(
-                                              task['due_date'],
-                                            ),
-                                          ),
-                                          style: const TextStyle(
-                                            fontSize: AppConstants.valueDouble12,
-                                          ),
-                                        ),
-                                      SubtaskSection(
-                                        controller: controller,
-                                        task: task,
-                                      ),
-                                    ],
+                            );
+                            controller.loadTasks();
+                          },
+                          child: Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  task['title'] ?? '',
+                                  style: TextStyle(
+                                    fontSize: AppConstants.valueDouble16,
+                                    decoration: task['is_completed'] == 1
+                                        ? TextDecoration.lineThrough
+                                        : null,
+                                    color: Colors.black,
                                   ),
                                 ),
-                              ),
+                                Text(
+                                  task['notes'] ?? '',
+                                  style: const TextStyle(
+                                    fontSize: AppConstants.valueDouble12,
+                                  ),
+                                ),
+                                if (task['due_date'] != null)
+                                  Text(
+                                    DateFormat('EEEE, MMM d, h:mm a').format(
+                                      DateTime.fromMillisecondsSinceEpoch(
+                                        task['due_date'],
+                                      ),
+                                    ),
+                                    style: const TextStyle(
+                                      fontSize: AppConstants.valueDouble12,
+                                    ),
+                                  ),
+                                SubtaskSection(
+                                  controller: controller,
+                                  task: task,
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
                       ),
                       if (task['is_completed'] != 1)
